fe545e9cb7853373dc7fafa9c5bcebbe314b3d6e
dennismoon <dmoon396@hotmail.com>
2017-02-01 16:10:09 -0600

    Added Step 6 - Add Swagger Examples

---
+++ Examples/Step 6 - Add Swagger/AspNetCoreWebApiBaseline/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace AspNetCoreWebApiBaseline
+{
+    public class Program
+    {
+        static public IConfigurationRoot Configuration { get; set; }
+
+        public static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();
+
+            var showDetailedErrors = Configuration["ShowDetailedErrors"];
+
+            var host = new WebHostBuilder()
+                .CaptureStartupErrors(true)
+                .UseSetting("detailedErrors", showDetailedErrors)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
