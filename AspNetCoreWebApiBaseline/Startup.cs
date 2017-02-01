efc1cdeb27b7d90961695e420b2ebf3673db6966
dennismoon <dmoon396@hotmail.com>
2017-01-31 21:25:22 -0600

    Added Error Handling to Program and Startup classes.

--- AspNetCoreWebApiBaseline/Startup.cs
+++ AspNetCoreWebApiBaseline/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -26,6 +22,7 @@ namespace AspNetCoreWebApiBaseline
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
             }
 
             app.Run(async (context) =>
