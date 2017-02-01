a1e5aab81a5ae982938d239abd6deb0a5edd1075
dennismoon <dmoon396@hotmail.com>
2017-02-01 08:54:47 -0600

    Createed Step 3 - Added Logging

---
+++ Examples/Step 3 - Add Logging/AspNetCoreWebApiBaseline/Startup.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using NLog.Web;
+
+namespace AspNetCoreWebApiBaseline
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            // Add various loggers
+            loggerFactory.AddConsole();
+
+            loggerFactory.AddDebug();
+
+            // Add NLog to .NET Core
+            loggerFactory.AddNLog();
+
+            // Enable ASP.NET Core features (NLog.web)
+            app.AddNLogWeb();
+
+            // Configure developer exception handling
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
